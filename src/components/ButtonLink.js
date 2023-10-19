f1a3ae3263cd8a54193912101b2556802e03b6f3
access123 <46967778+access123@users.noreply.github.com>
2023-10-19 11:04:09 +0530

    Update ButtonLink.js

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,11 +1,51 @@
 import React from "react";
 import "./styles/ButtonLink.css";
+import { Routes, Route } from 'react-router-dom';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Side from './Side';
+import IconButton from '@mui/material/IconButton';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+ const ButtonLink = ({ text = "Button", className, divClassName, onClick  }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
- const ButtonLink = ({ text = "Button", className, divClassName }) => {
-  return (
-    <button className={`button-link ${className}`}>
+  const openDrawer = () => {
+    setDrawerOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
+  const handleClick = () =>{
+        openDrawer();
+  }
+  return (<>
+   <Routes>
+        <Route path='side' element={<Side />} />
+      </Routes>
+   <button className={`button-link ${className}`}>
       <div className={`button ${divClassName}`}>{text}</div>
     </button>
+  <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={closeDrawer}
+        className='Sidebar'
+      >
+        <div
+          role="presentation"
+          onClick={closeDrawer}
+          onKeyDown={closeDrawer}
+        >
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <List>
+            <Side />
+          </List>
+        </div>
+      </Drawer>
+   </>
   );
 };
 
