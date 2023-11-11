13555925298f6194941d1ed100418817a4cbed6a
DevPanchal1 <145889118+DevPanchal1@users.noreply.github.com>
2023-11-11 07:43:34 +0000

    LogOut

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
 import React from 'react';
 import Login from "./components/Login"
@@ -9,12 +10,26 @@ import Guide from './components/Guide';
 import Admin from './components/Admin';
 import Undercon from './components/Undercon';
 import UserProvider from './components/Context';
+=======
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import Courses from "./components/Courses";
+import Roadmap1 from "./components/Roadmap1";
+import Guide from "./components/Guide";
+import Admin from "./components/Admin";
+import Undercon from "./components/Undercon";
+import UserProvider, { UserContext } from "./components/Context";
+import Quiz from "./components/Quiz";
+>>>>>>> origin
 
 export default function App() {
   return (
     <UserProvider>
       <Routes>
-        <Route path="/"  element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/contact" element={<Undercon />} />
         <Route path="/login" element={<Login />} />
@@ -22,8 +37,8 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/Courses" element={<Courses />} />
         <Route path="/guides" element={<Guide />} />
-        <Route path="/quiz" element={<Undercon />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </UserProvider>
-  )
+  );
 }
