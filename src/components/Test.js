85f32c20db98ea7ffaf715cf7f905476c3b76df9
Kshitij Patil <kshitij2707@gmail.com>
2023-11-14 18:10:36 +0530

    Changes

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Nav from './Nav'
-import './styles/Test.css'
-import CountdownTimer from './CountdownTimer';
-import Quiz from './Assets/Quiz.json'
+import React, { useEffect, useState } from "react";
+import Nav from "./Nav";
+import "./styles/Test.css";
+import CountdownTimer from "./CountdownTimer";
+import Quiz from "./Assets/Quiz.json";
 import { RadioGroup } from "@mui/joy";
-import RenderedOptions from './RenderedOptions'
+import RenderedOptions from "./RenderedOptions";
 
 function Test() {
   useEffect(() => {
-    if (CountdownTimer === 0 || questionNo === 20) {
+    if (CountdownTimer === 0 || questionNo === 10) {
       endTest();
     }
-  }, [CountdownTimer])
-  const endTest = () =>{
-    console.log('Hello')
-  }
+  }, [CountdownTimer]);
+
+  const endTest = () => {
+    console.log("Hello");
+  };
   const [questionNo, setQuestionNo] = useState(1);
   const quizArray = Quiz;
   const randomQuestion = () => {
     const useQuestion = quizArray[Math.floor(Math.random() * 20) + 1];
     return useQuestion;
-  }
+  };
   function handleOptionChange(event) {
     setSelectedOption(event.target.value);
   }
@@ -29,55 +30,58 @@ function Test() {
   const answerobj = [];
   return (
     <>
-      <Nav titleLink={''} li1={"HOME"} li2={"ABOUT"} li3={"CONTACT"} />
-      <h1 style={{ marginLeft: '45.5%' }}>TEST</h1>
-      <h4 style={{ marginLeft: '40%' }}>Quiz on Java Module 1</h4>
+      <Nav titleLink={""} li1={"HOME"} li2={"ABOUT"} li3={"CONTACT"} />
+      <h1 style={{ marginLeft: "45.5%" }}>TEST</h1>
+      <h4 style={{ marginLeft: "40%" }}>Quiz on Java Module 1</h4>
       <div className="container">
         <div className="flex">
           <p>
-            Question <span id="qno">{questionNo}</span> of
-            10
+            Question <span id="qno">{questionNo}</span> of 10
           </p>
           <CountdownTimer />
-          <h1>
-            Questions
-          </h1>
+          <h1>Questions</h1>
         </div>
       </div>
       <div className="quiz-box">
-
         <div className="mcqs">
           <div className="padding">
             <p>{question.question}</p>
           </div>
-          <div >
+          <div>
             <RadioGroup
               aria-labelledby="completion-label"
               value={selectedOption}
               onChange={handleOptionChange}
               size="md"
-              sx={{ gap: 1, flexDirection: 'row' }}
+              sx={{ gap: 1, flexDirection: "row" }}
             >
               <RenderedOptions questionInfo={question} />
             </RadioGroup>
             <hr />
           </div>
-          <div className='padding btn-group'>
-            <button className='btn btn-outline-info btn-sm' onClick={() => {
-              setQuestion(randomQuestion());
-              setQuestionNo(questionNo + 1);
-              
-            }}>
+          <div className="padding btn-group">
+            <button
+              className="btn btn-outline-info btn-sm"
+              onClick={() => {
+                setQuestion(randomQuestion());
+                setQuestionNo(questionNo + 1);
+              }}
+            >
               Next
             </button>
-            <button className='btn btn-outline-info btn-sm mx-2' onClick={() => {
-              endTest();
-            }}>End Test</button>
+            <button
+              className="btn btn-outline-info btn-sm mx-2"
+              onClick={() => {
+                endTest();
+              }}
+            >
+              End Test
+            </button>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Test;
