2c202f02a0329747af96d8d400e00ad381fd24bc
DevPanchal1 <145889118+DevPanchal1@users.noreply.github.com>
2023-11-15 11:39:03 +0000

    Quiz is working fine i think.

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Nav from "./Nav";
 import "./styles/Test.css";
-import CountdownTimer from "./CountdownTimer";
 import Quiz from "./Assets/Quiz.json";
 import { RadioGroup, Sheet, Radio } from "@mui/joy";
 
 function Test() {
+  const [timeInSeconds, setTimeInSeconds] = useState(600);
+  const [notEndTest, setnotEndTest] = useState(true);
   useEffect(() => {
-    if (CountdownTimer === 0 || questionNo === 11) {
+    const timer = setInterval(() => {
+      if (timeInSeconds > 0) {
+        setTimeInSeconds((prevTime) => prevTime - 1);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [timeInSeconds]);
+
+  const formatTime = () => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  useEffect(() => {
+    if (timeInSeconds === 0) {
       endTest();
     }
-  }, [CountdownTimer]);
+  }, [timeInSeconds]);
 
   const quizArray = [];
   for (var i = 0; i < 10; i++) {
     quizArray.push(Quiz[i]);
   }
-
+  var forResult =['Questions','Result'] 
   const [questionNo, setQuestionNo] = useState(1);
   const [question, setQuestion] = useState(quizArray[questionNo - 1]);
   const [selectedOption, setSelectedOption] = useState(-1);
@@ -46,6 +62,7 @@ function Test() {
       return true;
     }
   };
+  const [scoreState, setScoreState] = useState(0);
   const endTest = () => {
     let score = 0;
     for (let i = 0; i < 10; i++) {
@@ -53,9 +70,9 @@ function Test() {
         score++;
       }
     }
-    console.log("Your Score:", score);
+    setScoreState(score);
+    setnotEndTest(false);
   };
-
 
   return (
     <>
@@ -64,100 +81,120 @@ function Test() {
       <h4 style={{ marginLeft: "40%" }}>Quiz on Java Module 1</h4>
       <div className="container">
         <div className="flex">
+          {notEndTest && (
+          <>
+            <h1>{forResult[0]}</h1>
           <p>
             Question <span id="qno">{questionNo}</span> of 10
           </p>
-          <CountdownTimer />
-          <h1>Questions</h1>
+          <div>
+            <div>{formatTime()}</div>
+            {timeInSeconds === 0 && <p>Time's up!</p>}
+          </div>
+          </>
+          )}
+          {!notEndTest && (
+            <>
+            <h1>{forResult[1]}</h1>
+            <h4>{`You Scored ${scoreState} out of 10`}</h4>
+            <h5>Your response has been recorded</h5>
+            </>
+          )}
         </div>
       </div>
       <div className="quiz-box">
-        <div className="mcqs">
-          <div className="padding">
-            <p>{question.question}</p>
-          </div>
-          <div>
-            <RadioGroup
-              aria-labelledby="completion-label"
-              value={selectedOption}
-              onChange={handleOptionChange}
-              size="md"
-              sx={{ gap: 1, flexDirection: "row" }}
-            >
-              {question.options.map((option, index) => (
-                <Sheet
-                  key={index}
-                  sx={{
-                    p: 1,
-                    borderRadius: "sm",
-                    boxShadow: "xs",
-                  }}
+        {notEndTest && (
+          <>
+            <div className="mcqs">
+              <div className="padding">
+                <p>{question.question}</p>
+              </div>
+              <div>
+                <RadioGroup
+                  aria-labelledby="completion-label"
+                  value={selectedOption}
+                  onChange={handleOptionChange}
+                  size="md"
+                  sx={{ gap: 1, flexDirection: "row" }}
                 >
-                  <Radio
-                    label={option}
-                    overlay
-                    disableIcon
-                    value={option}
-                    slotProps={{
-                      label: ({ checked }) => ({
-                        sx: {
-                          fontWeight: "md",
-                          fontSize: "sm",
-                          color: checked ? "text.primary" : "text.secondary",
-                        },
-                      }),
-                      action: ({ checked }) => ({
-                        sx: (theme) => ({
-                          ...(checked && {
-                            "--variant-borderWidth": "1px",
-                            "&&": {
-                              borderColor: theme.vars.palette.primary[500],
+                  {question.options.map((option, index) => (
+                    <Sheet
+                      key={index}
+                      sx={{
+                        p: 1,
+                        borderRadius: "sm",
+                        boxShadow: "xs",
+                      }}
+                    >
+                      <Radio
+                        label={option}
+                        overlay
+                        disableIcon
+                        value={option}
+                        slotProps={{
+                          label: ({ checked }) => ({
+                            sx: {
+                              fontWeight: "md",
+                              fontSize: "sm",
+                              color: checked
+                                ? "text.primary"
+                                : "text.secondary",
                             },
                           }),
-                        }),
-                      }),
+                          action: ({ checked }) => ({
+                            sx: (theme) => ({
+                              ...(checked && {
+                                "--variant-borderWidth": "1px",
+                                "&&": {
+                                  borderColor: theme.vars.palette.primary[500],
+                                },
+                              }),
+                            }),
+                          }),
+                        }}
+                      />
+                    </Sheet>
+                  ))}
+                </RadioGroup>
+                <hr />
+              </div>
+              <div className="padding btn-group">
+                {renderToggleBack() && (
+                  <button
+                    className="btn btn-outline-info btn-sm mx-2"
+                    onClick={() => {
+                      setSelectedOption(answersArr[questionNo - 2]);
+                      setQuestionNo(questionNo - 1);
+                      setQuestion(quizArray[questionNo - 2]);
                     }}
-                  />
-                </Sheet>
-              ))}
-            </RadioGroup>
-            <hr />
-          </div>
-          <div className="padding btn-group">
-            {renderToggleBack() && (
-              <button
-                className="btn btn-outline-info btn-sm mx-2"
-                onClick={() => {
-                  setSelectedOption(answersArr[questionNo - 2]);
-                  setQuestionNo(questionNo - 1);
-                  setQuestion(quizArray[questionNo - 2]);
-                }}
-              >
-                Back
-              </button>
-            )}
-            {renderToggleNext() && (
-              <button
-                className="btn btn-outline-info btn-sm"
-                onClick={() => {
-                  setSelectedOption(answersArr[questionNo]);
-                  setQuestionNo(questionNo + 1);
-                  setQuestion(quizArray[questionNo]);
-                }}
-              >
-                Next
-              </button>
-            )}
-            <button
-              className="btn btn-outline-info btn-sm mx-2"
-              onClick={() => {
-                endTest();
-              }}
-            >
-              End Test
-            </button>
-          </div>
-        </div>
+                  >
+                    Back
+                  </button>
+                )}
+                {renderToggleNext() && (
+                  <button
+                    className="btn btn-outline-info btn-sm"
+                    onClick={() => {
+                      setSelectedOption(answersArr[questionNo]);
+                      setQuestionNo(questionNo + 1);
+                      setQuestion(quizArray[questionNo]);
+                    }}
+                  >
+                    Next
+                  </button>
+                )}
+                <button
+                  className="btn btn-outline-info btn-sm mx-2"
+                  onClick={() => {
+                    endTest();
+                  }}
+                >
+                  End Test
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
