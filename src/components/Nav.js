1eb50c62d43d93888287c459412c4ed7bfaff3f3
DevPanchal1 <145889118+DevPanchal1@users.noreply.github.com>
2023-11-17 10:37:06 +0000

    OK

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,107 +5,97 @@ import { Menu, MenuButton, MenuItem, Dropdown } from "@mui/joy";
 import { MoreVert } from "@mui/icons-material";
 import IconButton from "@mui/joy/IconButton";
 import { useUser } from "./Context";
+
 export default function Nav() {
   const location = useLocation();
   const [Toggle, setToggle] = useState(true);
-  const { isLoggedIn, user } = useUser();
+  const { isLoggedIn } = useUser();
+
   useEffect(() => {
     if (location.pathname === "/login" || location.pathname === "/signup") {
       setToggle(false);
     }
   }, [location.pathname]);
-  useEffect(() => {
-    if (isLoggedIn) {
-      const userDataFromSessionStorage = sessionStorage.getItem("email");
-      if (userDataFromSessionStorage) {
-        const userData = JSON.parse(userDataFromSessionStorage);
-      }
-    }
-  }, [isLoggedIn]);
+
   const logout = () => {
+    // Assuming you want to close the current window on logout
     window.close();
   };
 
   return (
-    <nav className="navbar navbar-expand-sm ">
-      <Link className="navbar-brand mx-5" to="/">
-        <img src="#" alt="LOGO" />
-      </Link>
-      <div className="">
-        <Link to="/Roadmap" className="navbar-brand">
-          ROADMAP
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand mx-5" to="/">
+          <img src="#" alt="LOGO" />
         </Link>
-      </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              HOME
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              ABOUT
-            </Link>
-          </li>
 
-          {Toggle && (
-            <>
-              <Dropdown>
-                <MenuButton
-                  slots={{ root: IconButton }}
-                  slotProps={{ root: { variant: "soft", color: "neutral" } }}
-                >
-                  <MoreVert />
-                </MenuButton>
-                <Menu>
-                  <MenuItem>
-                    <Link className="dropdown-item" to="/login">
-                      Login
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link className="dropdown-item" to="/signup">
-                      Signup
-                    </Link>
-                  </MenuItem>
-                </Menu>
-              </Dropdown>
-            </>
-          )}
-          {isLoggedIn && (
-            <>
-              <Dropdown>
-                <MenuButton
-                  slots={{ root: IconButton }}
-                  slotProps={{ root: { variant: "soft", color: "neutral" } }}
-                >
-                  <MoreVert />
-                </MenuButton>
-                <Menu>
-                  <MenuItem>
-                    <button
-                      onClick={() => {
-                        logout();
-                      }}
-                      className="links"
-                    >
-                      Log Out
-                    </button>
-                  </MenuItem>
-                </Menu>
-              </Dropdown>
-            </>
-          )}
-        </ul>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                ABOUT
+              </Link>
+            </li>
+
+            {Toggle && (
+              <>
+                <Dropdown>
+                  <MenuButton
+                    slots={{ root: IconButton }}
+                    slotProps={{ root: { variant: "soft", color: "neutral" } }}
+                  >
+                    <MoreVert />
+                  </MenuButton>
+                  <Menu>
+                    <MenuItem>
+                      <Link className="dropdown-item" to="/login">
+                        Login
+                      </Link>
+                    </MenuItem>
+                    <MenuItem>
+                      <Link className="dropdown-item" to="/signup">
+                        Signup
+                      </Link>
+                    </MenuItem>
+                  </Menu>
+                </Dropdown>
+              </>
+            )}
+            {isLoggedIn && (
+              <>
+                <Dropdown>
+                  <MenuButton
+                    slots={{ root: IconButton }}
+                    slotProps={{ root: { variant: "soft", color: "neutral" } }}
+                  >
+                    <MoreVert />
+                  </MenuButton>
+                  <Menu>
+                    <MenuItem>
+                      <button onClick={logout} className="links dropdown-item">
+                        Log Out
+                      </button>
+                    </MenuItem>
+                  </Menu>
+                </Dropdown>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
